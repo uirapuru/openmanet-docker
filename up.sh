2dc1d234508d8a7a370a85f0dca5a250d7e76537
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2025-11-12 20:48:43 +0100

    up

--- up.sh
+++ up.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
-HALF_CPUS=$(python3 - <<'PY'
-import os
-n = os.cpu_count() or 1
-print(max(1, n//2))
-PY
-)
-export HOST_UID=$(id -u) HOST_GID=$(id -g) HALF_CPUS
+
+if [ "${USE_ALL_CPUS:-0}" = "1" ]; then
+  CPUS=$(python3 -c 'import os; print(os.cpu_count() or 1)')
+else
+  CPUS=$(python3 -c 'import os; n=os.cpu_count() or 1; print(max(1, n//2))')
+fi
+
+export HOST_UID=$(id -u) HOST_GID=$(id -g) HALF_CPUS=$CPUS
 docker compose up --build
